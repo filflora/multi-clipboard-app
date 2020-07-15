b78c56faf679c2119ba519070951e5e29d396cd5
Fil <whirlion@gmail.com>
2020-07-15 21:22:05 +0200

    Fixed high cpu usage problem

--- src/sample/ClipboardListener.java
+++ src/sample/ClipboardListener.java
@@ -49,7 +49,13 @@ public class ClipboardListener extends Thread implements ClipboardOwner {
         Transferable transferable = clipboard.getContents(this);
         repairOwnership(transferable);
 
-        while (true);
+        while (true) {
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 }
